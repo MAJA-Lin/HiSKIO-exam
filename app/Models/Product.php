ff81b113b3acf84d7364898b86e996f932ece4c0
Scott Lin <josh.al.carrer@gmail.com>
2019-04-16 02:37:22 +0800

    Refactor to follow question description

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,23 +2,53 @@
 
 namespace App\Models;
 
-use App\Factories\PlatformFactory;
+use App\Models\Platforms\Platform;
+use App\Factories\PlatformFactoryInterface;
 
 class Product
 {
     /**
+     * @var array
+     */
+    protected $platforms = [];
+
+    /**
+     * @param PlatformFactoryInterface $factory
+     * @return Platform
+     */
+    public function getPlatform(PlatformFactoryInterface $factory): Platform
+    {
+        return $factory->createPlatform();
+    }
+
+    /**
+     * Setup platform(s) by given array
+     *
+     * @param array $platformClasses
+     * @return this
+     */
+    public function setupPlatforms(array $platformClasses): self
+    {
+        var_dump($platformClasses);
+
+        // Reset platforms to avoid problems
+        $this->platforms = [];
+
+        foreach ($platformClasses as $platformClass) {
+            $this->platforms[] = $this->getPlatform($platformClass);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Publish product information to platforms
      * @return void
      */
     public function publish()
     {
-        //
-    }
-
-    /**
-     * @return void
-     */
-    public function setupPlatform()
-    {
-        //
+        foreach ($this->platforms as $platform) {
+            $platform->publish($this);
+        }
     }
 }
--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -3,56 +3,28 @@
 namespace App\Services;
 
 use App\Models\Product;
-use App\Models\Platforms\Platform;
 
-use App\Factories\PlatformFactoryInterface;
+# Platforms, can add new platforms here.
+// use App\Factories\YahooFactory;
 use App\Factories\PchomeFactory;
 use App\Factories\ShopeeFactory;
 use App\Factories\RutenFactory;
-use App\Factories\YahooFactory;
 
 class ProductService
 {
-    /**
-     * @param PlatformFactoryInterface $factory
-     * @return Platform
-     */
-    public function getPlatform(PlatformFactoryInterface $factory): Platform
-    {
-        return $factory->createPlatform();
-    }
-
-    /**
-     * Setup platform(s) by given array
-     *
-     * @param array $platformClasses
-     * @return array
-     */
-    public function setupPlatform(array $platformClasses): array
-    {
-        $platforms = [];
-        foreach ($platformClasses as $platformClass) {
-            $platforms[] = $this->getPlatform($platformClass);
-        }
-
-        return $platforms;
-    }
-
     /**
      * @param Product $product Product which is going to be published
      * @return void
      */
     public function publishNewProduct(Product $product): void
     {
-        // select platform to publish
-        $platforms = $this->setupPlatform([
+        // Attach platforms to publish
+        $product->setupPlatforms([
             PchomeFactory::class,
             ShopeeFactory::class,
             RutenFactory::class
         ]);
 
-        foreach ($platforms as $platform) {
-            $platform->publish($product);
-        }
+        $product->publish();
     }
 }
